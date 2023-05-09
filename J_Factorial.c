c3938471b092a63bacbb594d10695504a49eb3f3
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-10 00:12:29 +0600

    contest: Sheet #7 (Recursion), problem: (J)

--- J_Factorial.c
+++ J_Factorial.c
@@ -1,9 +1,31 @@
 #include<stdio.h>
+long long int fact(long long int n)  //phitron's code//
+{
+    // base case
+    if(n==0)
+    {
+        return 1;
+    }
+    long long int ans=fact(n-1);
+    return ans*n;
+}
+int main()
+{
+    long long int n;
+    scanf("%lld",&n);
+    long long int ans=fact(n);
+    printf("%lld",ans);
+    return 0;
+}
+
+
+/*
+#include<stdio.h>
 long long fact(int n)
 {
-    if(n==0) return 1;
+    if(n==0) return 1; //factorial of 0 is 1
    //long long ans = fact(n-1);
-    return fact(n-1)*n;
+    return n*fact(n-1);
 }
 int main()
 {
@@ -14,3 +36,5 @@ int main()
     
     return 0;
 }
+
+*/
