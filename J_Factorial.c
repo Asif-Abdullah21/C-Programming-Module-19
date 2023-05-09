52693465c161a5b459a1fe23e5e672d54de6fee0
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-09 22:48:47 +0600

    Practice Sheet - recursion(J)

--- J_Factorial.c
+++ J_Factorial.c
@@ -2,15 +2,15 @@
 long long fact(int n)
 {
     if(n==0) return 1;
-    long long ans = fact(n-1);
-    return ans*n;
+   //long long ans = fact(n-1);
+    return fact(n-1)*n;
 }
 int main()
 {
     int n;
     scanf("%d",&n);
     long long ans = fact(n);
-    printf("%d\n",ans);
+    printf("%lld\n",ans);
     
     return 0;
 }
