389fdaed9c48c7ceb2ca90fc5a2c0cda46481382
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-10 00:26:38 +0600

    Sheet #7 (Recursion), problem: (K)

--- K_Max_Number.c
+++ K_Max_Number.c
@@ -1,4 +1,34 @@
 #include<stdio.h>
+#include<limits.h> //header file for INT_MIN
+
+int max(int a[],int n,int i)
+{
+    if(i==n) return INT_MIN;
+    int mx = max(a,n,i+1);
+    if(a[i]>mx) return a[i];
+    else return mx;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    int ans = max(a,n,0);
+    printf("%d\n",ans);
+    
+    return 0;
+}
+
+
+/*
+#include<stdio.h>
 
 int max(int *a,int i)
 {
@@ -24,3 +54,5 @@ int main()
     
     return 0;
 }
+
+*/
